0513ddfb59fa3770dd5f05e563cbf9cb5db306f0
Lưu Mạnh Quân <>
2022-05-04 23:04:12 +0700

    fix bug

--- lib/tien_ich_module/presentation/danh_sach_cong_viec/ui/tablet/danh_sach_cong_viec_tien_ich_tablet.dart
+++ lib/tien_ich_module/presentation/danh_sach_cong_viec/ui/tablet/danh_sach_cong_viec_tien_ich_tablet.dart
@@ -1,4 +1,5 @@
 import 'package:ccvc_mobile/config/resources/styles.dart';
+import 'package:ccvc_mobile/config/themes/app_theme.dart';
 import 'package:ccvc_mobile/generated/l10n.dart';
 import 'package:ccvc_mobile/home_module/domain/model/home/todo_model.dart';
 import 'package:ccvc_mobile/home_module/presentation/home_screen/ui/tablet/widgets/scroll_bar_widget.dart';
@@ -15,6 +16,7 @@ import 'package:ccvc_mobile/tien_ich_module/widget/appbar/app_bar_with_two_leadi
 import 'package:ccvc_mobile/tien_ich_module/widget/search/base_search_bar.dart';
 import 'package:ccvc_mobile/utils/constants/image_asset.dart';
 import 'package:ccvc_mobile/utils/extensions/date_time_extension.dart';
+import 'package:ccvc_mobile/utils/extensions/string_extension.dart';
 import 'package:ccvc_mobile/widgets/show_buttom_sheet/show_bottom_sheet.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
@@ -68,7 +70,7 @@ class _DanhSachCongViecTienIchTabletState
             shape: const RoundedRectangleBorder(
               borderRadius: BorderRadius.all(Radius.circular(10.0)),
             ),
-            icon: SvgPicture.asset(ImageAssets.ic_Group),
+            icon: SvgPicture.asset(ImageAssets.ic_Group.svgToTheme()),
             itemBuilder: (context) => [
               PopupMenuItem(
                 child: AddToDoWidgetTienIch(
@@ -198,7 +200,7 @@ class _DanhSachCongViecTienIchTabletState
                                       child: Text(
                                         S.current.chon_ngay,
                                         style: textNormalCustom(
-                                          color: dangXyLyColor,
+                                          color: AppTheme.getInstance().colorField(),
                                           fontSize: 14,
                                         ),
                                       ),

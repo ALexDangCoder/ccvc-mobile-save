cbf7e32355360b3770f04ca4d5c3c0f68822b9d9
hungtv <hungtv@edsolabs.com>
2022-06-07 17:52:50 +0700

    finish menu phone+tablet diem danh

---
+++ lib/diem_danh_module/presentation/bloc/diem_danh_state.dart
@@ -0,0 +1,27 @@
+import 'package:equatable/equatable.dart';
+
+abstract class DiemDanhState extends Equatable {
+  const DiemDanhState();
+}
+
+class DiemDanhStateIntial extends DiemDanhState {
+  const DiemDanhStateIntial() : super();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class DiemDanhCaNhan extends DiemDanhState {
+  @override
+  List<Object?> get props => [];
+}
+
+class DiemDanhKhuonMat extends DiemDanhState {
+  @override
+  List<Object?> get props => [];
+}
+
+class DiemDanhBienSoXe extends DiemDanhState {
+  @override
+  List<Object?> get props => [];
+}

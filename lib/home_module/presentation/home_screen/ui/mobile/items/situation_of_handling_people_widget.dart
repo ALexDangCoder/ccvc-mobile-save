24bd44002cb99048ff1059b93206bb3f2bd94045
Lưu Mạnh Quân <>
2022-06-08 10:15:11 +0700

    fix bug pakn

--- lib/home_module/presentation/home_screen/ui/mobile/items/situation_of_handling_people_widget.dart
+++ lib/home_module/presentation/home_screen/ui/mobile/items/situation_of_handling_people_widget.dart
@@ -1,20 +1,21 @@
 import 'package:ccvc_mobile/home_module/config/resources/color.dart';
 import 'package:ccvc_mobile/home_module/config/resources/styles.dart';
 import 'package:ccvc_mobile/home_module/domain/model/home/document_dashboard_model.dart';
+import 'package:ccvc_mobile/home_module/presentation/home_screen/ui/widgets/status_column_chart.dart';
 import 'package:ccvc_mobile/home_module/presentation/home_screen/ui/widgets/status_widget.dart';
 import 'package:ccvc_mobile/home_module/utils/constants/app_constants.dart';
-import 'package:ccvc_mobile/utils/extensions/size_extension.dart';
+
 import 'package:flutter/material.dart';
 
 import '/generated/l10n.dart';
 import '/home_module/domain/model/home/WidgetType.dart';
-import '/home_module/domain/model/home/tinh_hinh_y_kien_model.dart';
+
 import '/home_module/presentation/home_screen/bloc/home_cubit.dart';
 import '/home_module/presentation/home_screen/ui/home_provider.dart';
 import '/home_module/presentation/home_screen/ui/mobile/widgets/container_backgroud_widget.dart';
-import '/home_module/presentation/home_screen/ui/widgets/dialog_setting_widget.dart';
+
 import '/home_module/widgets/chart/base_pie_chart.dart';
-import '/home_module/widgets/text/text/no_data_widget.dart';
+
 import '/home_module/widgets/text/views/loading_only.dart';
 
 class TinhHinhPAKNCuaCaNhanMobileWidget extends StatefulWidget {
@@ -77,55 +78,38 @@ class _SituationOfHandlingPeopleWidgetState
                       ),
                     ),
                   ),
-                  Stack(
-                    children: [
-                      SizedBox(
-                        height: 260,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: List.generate(
-                            8,
-                            (index) => const MySeparator(
-                              color: lineColor,
-                              height: 2,
-                            ),
-                          ),
-                        ),
-                      ),
-                      statusWidget([
-                        ChartData(
-                          S.current.cho_tiep_nhan,
-                          data.soLuongChoTiepNhan.toDouble(),
-                          choTrinhKyColor,
-                          SelectKey.CHO_TRINH_KY,
-                        ),
-                        ChartData(
-                          S.current.phan_xu_ly,
-                          data.soLuongPhanXuLy.toDouble(),
-                          numberOfCalenders,
-                          SelectKey.CHO_XU_LY,
-                        ),
-                        ChartData(
-                          S.current.dang_xu_ly,
-                          data.soLuongDangXuLy.toDouble(),
-                          daXuLyColor,
-                          SelectKey.DA_XU_LY,
-                        ),
-                        ChartData(
-                          S.current.cho_duyet,
-                          data.soLuongChoDuyet.toDouble(),
-                          choCapSoColor,
-                          SelectKey.CHO_CAP_SO,
-                        ),
-                        ChartData(
-                          S.current.cho_bo_sung_thong_tin,
-                          data.soLuongChoBoSungThongTin.toDouble(),
-                          choBanHanhColor,
-                          SelectKey.CHO_BAN_HANH,
-                        )
-                      ]),
-                    ],
-                  ),
+                  StatusColumnChart(listData: [
+                    ChartData(
+                      S.current.cho_tiep_nhan,
+                      data.soLuongChoTiepNhan.toDouble(),
+                      choTrinhKyColor,
+                      SelectKey.CHO_TRINH_KY,
+                    ),
+                    ChartData(
+                      S.current.phan_xu_ly,
+                      data.soLuongPhanXuLy.toDouble(),
+                      numberOfCalenders,
+                      SelectKey.CHO_XU_LY,
+                    ),
+                    ChartData(
+                      S.current.dang_xu_ly,
+                      data.soLuongDangXuLy.toDouble(),
+                      daXuLyColor,
+                      SelectKey.DA_XU_LY,
+                    ),
+                    ChartData(
+                      S.current.cho_duyet,
+                      data.soLuongChoDuyet.toDouble(),
+                      choCapSoColor,
+                      SelectKey.CHO_CAP_SO,
+                    ),
+                    ChartData(
+                      S.current.cho_bo_sung_thong_tin,
+                      data.soLuongChoBoSungThongTin.toDouble(),
+                      choBanHanhColor,
+                      SelectKey.CHO_BAN_HANH,
+                    )
+                  ]),
                   const SizedBox(
                     height: 24,
                   ),
@@ -220,127 +204,7 @@ class _SituationOfHandlingPeopleWidgetState
   }
 }
 
-Widget statusWidget(List<ChartData> listData) {
-  final data = listData.map((e) => e.value).toList();
-  final total = data.reduce((a, b) => a + b);
-  return Column(
-    mainAxisSize: MainAxisSize.min,
-    children: [
-      Container(
-        padding: const EdgeInsets.symmetric(horizontal: 14),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: listData
-              .map(
-                (e) => Row(
-                  children: [
-                    Container(
-                      height: 260,
-                      width: 38,
-                      alignment: Alignment.bottomCenter,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: lineColor,
-                      ),
-                      // color: e.color,
-                      child: e.value.toInt() == 0
-                          ? FittedBox(
-                              child: Text(
-                                e.value.toInt().toString(),
-                                style: textNormal(
-                                  textTitleColumn,
-                                  14.0.textScale(),
-                                ),
-                              ),
-                            )
-                          : Column(
-                              children: [
-                                Expanded(
-                                  flex: (total - (e.value)).toInt(),
-                                  child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.end,
-                                    children: [
-                                      FittedBox(
-                                        child: Text(
-                                          e.value.toInt().toString(),
-                                          style: textNormal(
-                                            textTitleColumn,
-                                            14.0.textScale(),
-                                          ),
-                                        ),
-                                      ),
-                                      const SizedBox(
-                                        height: 6,
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                                Expanded(
-                                  flex: e.value.toInt(),
-                                  child: Container(
-                                    width: 38,
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(10),
-                                      color: e.color,
-                                    ),
-                                  ),
-                                )
-                              ],
-                            ),
-                    ),
-                  ],
-                ),
-              )
-              .toList(),
-        ),
-      ),
-      const SizedBox(
-        height: 24,
-      ),
-      GridView.count(
-        shrinkWrap: true,
-        physics: const NeverScrollableScrollPhysics(),
-        crossAxisCount: 2,
-        childAspectRatio: 9,
-        mainAxisSpacing: 10.0.textScale(space: 4),
-        crossAxisSpacing: 10,
-        children: List.generate(listData.length, (index) {
-          final result = listData[index];
-          // ignore: avoid_unnecessary_containers
-          return GestureDetector(
-            onTap: () {},
-            child: Row(
-              children: [
-                Container(
-                  height: 14,
-                  width: 14,
-                  decoration: BoxDecoration(
-                    color: result.color,
-                    shape: BoxShape.circle,
-                  ),
-                ),
-                const SizedBox(
-                  width: 12,
-                ),
-                Flexible(
-                  child: FittedBox(
-                    child: Text(
-                      '${result.title} (${result.value.toInt()})',
-                      style: textNormal(
-                        infoColor,
-                        14.0.textScale(),
-                      ),
-                    ),
-                  ),
-                )
-              ],
-            ),
-          );
-        }),
-      ),
-    ],
-  );
-}
+
 
 class MySeparator extends StatelessWidget {
   const MySeparator({Key? key, this.height = 1, this.color = Colors.black})
---
+++ lib/home_module/presentation/home_screen/ui/widgets/status_column_chart.dart
@@ -0,0 +1,152 @@
+
+import 'package:ccvc_mobile/config/resources/styles.dart';
+import 'package:ccvc_mobile/home_module/config/resources/color.dart';
+import 'package:ccvc_mobile/home_module/presentation/home_screen/ui/mobile/items/situation_of_handling_people_widget.dart';
+import 'package:ccvc_mobile/home_module/widgets/chart/base_pie_chart.dart';
+import 'package:ccvc_mobile/utils/extensions/size_extension.dart';
+import 'package:flutter/material.dart';
+class StatusColumnChart extends StatelessWidget {
+  final List<ChartData> listData;
+  const StatusColumnChart({Key? key,required this.listData}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final data = listData.map((e) => e.value).toList();
+    final total = data.reduce((a, b) => a + b);
+    return Stack(
+      children: [
+        SizedBox(
+          height: 260,
+          child: Column(
+            mainAxisAlignment:
+            MainAxisAlignment.spaceBetween,
+            children: List.generate(
+              8,
+                  (index) => const MySeparator(
+                color: lineColor,
+                height: 2,
+              ),
+            ),
+          ),
+        ),
+        Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 14),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: listData
+                    .map(
+                      (e) => Row(
+                    children: [
+                      Container(
+                        height: 260,
+                        width: 38,
+                        alignment: Alignment.bottomCenter,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: lineColor,
+                        ),
+                        // color: e.color,
+                        child: e.value.toInt() == 0
+                            ? FittedBox(
+                          child: Text(
+                            e.value.toInt().toString(),
+                            style: textNormal(
+                              textTitleColumn,
+                              14.0.textScale(),
+                            ),
+                          ),
+                        )
+                            : Column(
+                          children: [
+                            Expanded(
+                              flex: (total - (e.value)).toInt(),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  FittedBox(
+                                    child: Text(
+                                      e.value.toInt().toString(),
+                                      style: textNormal(
+                                        textTitleColumn,
+                                        14.0.textScale(),
+                                      ),
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    height: 6,
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Expanded(
+                              flex: e.value.toInt(),
+                              child: Container(
+                                width: 38,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: e.color,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+                    .toList(),
+              ),
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+            GridView.count(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              crossAxisCount: 2,
+              childAspectRatio: 9,
+              mainAxisSpacing: 10.0.textScale(space: 4),
+              crossAxisSpacing: 10,
+              children: List.generate(listData.length, (index) {
+                final result = listData[index];
+                // ignore: avoid_unnecessary_containers
+                return GestureDetector(
+                  onTap: () {},
+                  child: Row(
+                    children: [
+                      Container(
+                        height: 14,
+                        width: 14,
+                        decoration: BoxDecoration(
+                          color: result.color,
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 12,
+                      ),
+                      Flexible(
+                        child: FittedBox(
+                          child: Text(
+                            '${result.title} (${result.value.toInt()})',
+                            style: textNormal(
+                              infoColor,
+                              14.0.textScale(),
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                );
+              }),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}

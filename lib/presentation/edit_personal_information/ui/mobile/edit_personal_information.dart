8c0b4cfd135b601095865f7224e9378307363c35
Lưu Mạnh Quân <>
2022-06-14 09:23:51 +0700

    fix bug reload home

--- lib/presentation/edit_personal_information/ui/mobile/edit_personal_information.dart
+++ lib/presentation/edit_personal_information/ui/mobile/edit_personal_information.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:ccvc_mobile/config/resources/styles.dart';
 import 'package:ccvc_mobile/config/themes/app_theme.dart';
 import 'package:ccvc_mobile/data/exception/app_exception.dart';
@@ -530,6 +532,7 @@ class _EditPersonalInformationScreen
                             )
                                 .then((value) {
                               if (value) {
+                                log("message");
                                 Navigator.pop(context, true);
                               }
                             });

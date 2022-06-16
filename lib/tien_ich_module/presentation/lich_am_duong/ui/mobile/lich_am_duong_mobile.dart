0084df503017b4b868290570ba91c79b9be8e5ee
lucha <Luchn@edsolabs.com>
2022-06-16 16:41:10 +0700

    update code

--- lib/tien_ich_module/presentation/lich_am_duong/ui/mobile/lich_am_duong_mobile.dart
+++ lib/tien_ich_module/presentation/lich_am_duong/ui/mobile/lich_am_duong_mobile.dart
@@ -202,7 +202,7 @@ class _LichAmDuongMobileState extends State<LichAmDuongMobile> {
                   return TableCalendarWidget(
                     isFomatMonth: isCheckOnTap,
                     onChange: (DateTime start, DateTime end, selectDay) {
-                      cubit.startDate = start.formatApi;
+                      cubit.startDate = start.formatApiDDMMYYYY;
                       cubit.getLichAmDuong(cubit.startDate);
                       cubit.selectTime = selectDay;
                     },

098a6a21c697a623789fcfd3c1434174f9b6c488
Nghĩa Nguyễn <>
2022-06-20 08:41:32 +0700

    up build version

--- lib/widgets/textformfield/text_field_validator.dart
+++ lib/widgets/textformfield/text_field_validator.dart
@@ -211,7 +211,7 @@ class AppCupertinoTextSelectionControls extends CupertinoTextSelectionControls {
             offset: value.selection.start + text.length,
           ),
         ),
-        SelectionChangedCause.toolBar,
+        SelectionChangedCause.toolbar,
       );
     }
     delegate.bringIntoView(delegate.textEditingValue.selection.extent);
@@ -253,7 +253,7 @@ class AppMaterialTextSelectionControls extends MaterialTextSelectionControls {
             offset: value.selection.start + text.length,
           ),
         ),
-        SelectionChangedCause.toolBar,
+        SelectionChangedCause.toolbar,
       );
     }
     delegate.bringIntoView(delegate.textEditingValue.selection.extent);

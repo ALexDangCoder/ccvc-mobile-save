6219966ab89ff3ac2321bea211d0969bb9f19073
doanhdd <>
2022-06-29 09:38:59 +0700

    Update menu ho tro ky thuat

---
+++ lib/ho_tro_ky_thuat_module/presentation/ho_tro_ky_thuat/mobile/ho_tro_ky_thuat_mobile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HoTroKyThuatMobile extends StatefulWidget {
+  const HoTroKyThuatMobile({Key? key}) : super(key: key);
+
+  @override
+  State<HoTroKyThuatMobile> createState() => _HoTroKyThuatMobileState();
+}
+
+class _HoTroKyThuatMobileState extends State<HoTroKyThuatMobile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

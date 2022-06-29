3058b003f072d0baa62b81f6e9fe0b43b71c56a1
Duy <vund.0709@gmail.com>
2022-06-29 10:12:57 +0700

    Bug: https://jira.edsolabs.com/browse/A22DAMPIMOBI-601 Root case: khi nhập số lượng quá 19 chữ số, dart không parse được sang kiểu số vì quá lớn Solution: Giới hạn tối đa nhập 19 chữ số

--- lib/presentation/chon_phong_hop/widgets/yeu_cau_them_thiet_bi_widget.dart
+++ lib/presentation/chon_phong_hop/widgets/yeu_cau_them_thiet_bi_widget.dart
@@ -194,6 +194,7 @@ class _ThemThietBiScreenState extends State<ThemThietBiScreen> {
                     textInputType: TextInputType.number,
                     controller: soLuong,
                     hintText: S.current.nhap_so_luong,
+                    maxLength: 19,
                     validator: (value) {
                       final result = (value ?? '').checkNull();
                       if (result != null) {

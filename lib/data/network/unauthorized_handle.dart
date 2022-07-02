9a2ee6f069daf16524d70d5dd49f28a2602b63ec
Lưu Mạnh Quân <>
2022-07-03 00:12:30 +0700

    handle unauthorized refesh token

---
+++ lib/data/network/unauthorized_handle.dart
@@ -0,0 +1,57 @@
+import 'package:ccvc_mobile/config/app_config.dart';
+import 'package:ccvc_mobile/data/exception/app_exception.dart';
+import 'package:ccvc_mobile/data/result/result.dart';
+import 'package:ccvc_mobile/domain/locals/prefs_service.dart';
+import 'package:ccvc_mobile/domain/model/account/data_user.dart';
+import 'package:ccvc_mobile/domain/repository/login_repository.dart';
+import 'package:get/get.dart';
+
+class HandleUnauthorized {
+  static final List<_ResultRefreshTokenCallBack> _callBackUnauthorized = [];
+  static void resignRefreshToken({
+    required Function(String) onRefreshToken,
+    required Function(AppException) onError,
+  }) {
+    if (_callBackUnauthorized.isEmpty) {
+      _handleUnauthorized().then((value) {
+        value.when(
+          success: (res)  {
+             PrefsService.saveToken(res.dataUser?.accessToken ?? '');
+             PrefsService.saveRefreshToken(
+              res.dataUser?.refreshToken ?? '',
+            );
+            for (final element in _callBackUnauthorized) {
+              element.onRefreshToken.call(res.dataUser?.accessToken ?? '');
+            }
+          },
+          error: (error) {
+            for (final element in _callBackUnauthorized) {
+              element.onError.call(error);
+            }
+          },
+        );
+      });
+    }
+    _callBackUnauthorized
+        .add(_ResultRefreshTokenCallBack(onRefreshToken, onError));
+  }
+
+  static Future<Result<DataLogin>> _handleUnauthorized() async {
+    final AccountRepository _loginRepo = Get.find();
+    //API LỖI
+    // await _loginRepo.refreshToken(
+    //   PrefsService.getToken(),
+    //   PrefsService.getRefreshToken(),
+    return _loginRepo.login(
+      PrefsService.getLoginUserName(),
+      PrefsService.getLoginPassWord(),
+      APP_CODE,
+    );
+  }
+}
+
+class _ResultRefreshTokenCallBack {
+  final Function(String) onRefreshToken;
+  final Function(AppException) onError;
+  _ResultRefreshTokenCallBack(this.onRefreshToken, this.onError);
+}

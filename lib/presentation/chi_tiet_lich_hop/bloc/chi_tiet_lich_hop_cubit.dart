25792e2a7dd981c92b42faf770a1f77656616a97
Duy <vund.0709@gmail.com>
2022-07-11 18:03:04 +0700

    Fix bug: https://jira.edsolabs.com/browse/A22DAMPIMOBI-757 Root case: Sau khi sửa lịch họp, back về main lịch họp không tự động refresh data Solution: Refresh data khi back về main lịch nếu sửa lịch họp thành công

--- lib/presentation/chi_tiet_lich_hop/bloc/chi_tiet_lich_hop_cubit.dart
+++ lib/presentation/chi_tiet_lich_hop/bloc/chi_tiet_lich_hop_cubit.dart
@@ -83,6 +83,8 @@ class DetailMeetCalenderCubit extends BaseCubit<DetailMeetCalenderState> {
   List<Data> listStatusRom = [];
   List<DonViModel> listDataCanBo = [];
   Timer? _debounce;
+  bool needRefreshMainMeeting = false;
+
   List<ButtonStatePhatBieu> buttonStatePhatBieu = [
     ButtonStatePhatBieu(
       key: S.current.danh_sach_phat_bieu,

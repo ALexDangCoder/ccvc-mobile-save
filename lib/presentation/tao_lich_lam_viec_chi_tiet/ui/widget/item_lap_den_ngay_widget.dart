e4747b04ad625db27002527134ed67eec9ffa5f4
namnguyenthanh <nam.nt0910@gmail.com>
2022-07-13 11:28:05 +0700

    modify time picker

--- lib/presentation/tao_lich_lam_viec_chi_tiet/ui/widget/item_lap_den_ngay_widget.dart
+++ lib/presentation/tao_lich_lam_viec_chi_tiet/ui/widget/item_lap_den_ngay_widget.dart
@@ -5,6 +5,7 @@ import 'package:ccvc_mobile/presentation/tao_lich_lam_viec_chi_tiet/bloc/create_
 import 'package:ccvc_mobile/utils/constants/image_asset.dart';
 import 'package:ccvc_mobile/utils/debouncer.dart';
 import 'package:ccvc_mobile/utils/extensions/date_time_extension.dart';
+import 'package:ccvc_mobile/widgets/calendar/cupertino_date_picker/cupertino_date_picker.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
@@ -84,20 +85,16 @@ class _ItemLapDenNgayWidgetState extends State<ItemLapDenNgayWidget> {
                     ),
                   ],
                 ),
+                spaceH10,
                 AnimatedContainer(
                   duration: const Duration(
                     milliseconds: 300,
                   ),
-                  height: isShowDatePicker ? 200 : 1,
+                  height: isShowDatePicker ? 300 : 1,
                   child: isShowDatePicker
-                      ? CupertinoDatePicker(
-                          maximumDate: DateTime(2099, 12, 30),
-                          maximumYear: 2099,
-                          minimumYear: DateTime.now().year,
+                      ? FlutterRoundedCupertinoDatePickerWidget(
                           minimumDate: DateTime.now(),
-                          backgroundColor: backgroundColorApp,
-                          mode: CupertinoDatePickerMode.date,
-                          use24hFormat: true,
+                          textStyleDate: textNormal(color3D5586, 16),
                           initialDateTime: widget.initDate,
                           onDateTimeChanged: (value) {
                             deboucer.run(() {

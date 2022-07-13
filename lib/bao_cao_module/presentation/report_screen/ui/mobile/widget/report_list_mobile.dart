5070e94d39908371f91843ac44980131b081b22c
doanhdd <>
2022-07-13 14:19:13 +0700

    Fix size phan report

--- lib/bao_cao_module/presentation/report_screen/ui/mobile/widget/report_list_mobile.dart
+++ lib/bao_cao_module/presentation/report_screen/ui/mobile/widget/report_list_mobile.dart
@@ -43,7 +43,7 @@ class ReportListMobile extends StatelessWidget {
                   mainAxisSpacing: 17,
                   crossAxisSpacing: 17,
                   childAspectRatio: 1.5,
-                  mainAxisExtent: 130,
+                  mainAxisExtent: 134,
                 ),
                 itemCount: listReport.length,
                 itemBuilder: (context, index) {

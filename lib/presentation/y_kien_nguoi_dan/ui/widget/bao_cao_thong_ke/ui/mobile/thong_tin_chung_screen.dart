5c8b09a3bea1092815b26b6ca925f6f863550afa
huyquang <taquanghuy0386356200@gmail.com>
2022-07-14 11:46:50 +0700

    fix bug 798

--- lib/presentation/y_kien_nguoi_dan/ui/widget/bao_cao_thong_ke/ui/mobile/thong_tin_chung_screen.dart
+++ lib/presentation/y_kien_nguoi_dan/ui/widget/bao_cao_thong_ke/ui/mobile/thong_tin_chung_screen.dart
@@ -635,7 +635,7 @@ class _ThongTinChungYKNDScreenState extends State<ThongTinChungYKNDScreen> {
   TextTrangThai statusTrangThai(int soNgayToiHan) {
     if (soNgayToiHan < 0) {
       return TextTrangThai(S.current.qua_han, statusCalenderRed);
-    } else if (soNgayToiHan >= 3) {
+    } else if (soNgayToiHan > 3) {
       return TextTrangThai(S.current.trong_han, choTrinhKyColor);
     } else {
       return TextTrangThai(S.current.den_han, choVaoSoColor);

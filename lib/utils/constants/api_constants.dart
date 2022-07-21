632df0764ead4e9df6e20c2f51ed1c4ebc6e45f4
haipn5991 <haipn@edsolabs.com>
2022-07-21 16:47:52 +0700

    fix conflict

--- lib/utils/constants/api_constants.dart
+++ lib/utils/constants/api_constants.dart
@@ -322,7 +322,6 @@ class ApiConstants {
   static const XOA_BIEU_QUYET = '/vpdt/api/BieuQuyet/xoa-bieu-quyet';
   static const DANH_SACH_CAN_BO_BIEU_QUYET =
       '/vpdt/api/BieuQuyet/get-danh-sach-canbo-bieuquyet';
-  static const DANH_SACH_CAN_BO_BIEU_QUYET = '/vpdt/api/BieuQuyet/get-danh-sach-canbo-bieuquyet';
   static const BAO_CAO_THONG_KE_VAN_BAN_DON_VI = '/qlvb/api/ioc/thong-ke-van-ban-don-vi-duoc-chon';
 
 

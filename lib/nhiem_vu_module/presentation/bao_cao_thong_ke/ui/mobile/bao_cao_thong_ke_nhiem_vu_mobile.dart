6cb5eaf40b4b9caaa0ae11b5187d0d50cd22c108
hungnguyen <hungnguyen0904199x@gmail.com>
2022-07-22 15:48:28 +0700

    - Fix conflict

--- lib/nhiem_vu_module/presentation/bao_cao_thong_ke/ui/mobile/bao_cao_thong_ke_nhiem_vu_mobile.dart
+++ lib/nhiem_vu_module/presentation/bao_cao_thong_ke/ui/mobile/bao_cao_thong_ke_nhiem_vu_mobile.dart
@@ -15,7 +15,6 @@ import 'package:ccvc_mobile/nhiem_vu_module/presentation/bao_cao_thong_ke/ui/wid
 import 'package:ccvc_mobile/nhiem_vu_module/presentation/nhiem_vu/bloc/nhiem_vu_cubit.dart';
 import 'package:ccvc_mobile/nhiem_vu_module/presentation/nhiem_vu/ui/menu/nhiem_vu_menu_mobile.dart';
 import 'package:ccvc_mobile/nhiem_vu_module/presentation/nhiem_vu/ui/mobile/bloc/danh_sach_cubit.dart';
-import 'package:ccvc_mobile/nhiem_vu_module/presentation/xem_luong_xu_ly/widgets/tree_view_widget.dart';
 import 'package:ccvc_mobile/nhiem_vu_module/utils/constants/image_asset.dart';
 import 'package:ccvc_mobile/nhiem_vu_module/utils/extensions/date_time_extension.dart';
 import 'package:ccvc_mobile/nhiem_vu_module/utils/extensions/screen_device_extension.dart';
@@ -224,7 +223,7 @@ class _BaoCaoThongKeNhiemVuMobileState
                             title: [
                               Expanded(
                                 child: Text(
-                                  S.current.nhiem_vu_don_vi_xu_ly,
+                                  S.current.bieu_do_12_thang,
                                   style: textNormalCustom(
                                     color: AppTheme.getInstance().titleColor(),
                                     fontSize: 16,
@@ -240,30 +239,146 @@ class _BaoCaoThongKeNhiemVuMobileState
                                 final isCheck = snapshot.data ?? false;
                                 return isCheck
                                     ? ChartWidget(
-                                        titleFlex: 1,
-                                        chartFlex: 5,
+                                        titleFlex: 2,
                                         listData: [
                                           [
-                                            ChartData('title', 11, Colors.red),
                                             ChartData(
-                                                'title', 11, Colors.black),
-                                            ChartData('title', 111, Colors.red)
+                                                'title', 11, choXuLyColor),
+                                            ChartData(
+                                                'title', 1, chuaThucHienColor),
+                                            ChartData(
+                                                'title', 3, dangThucHienColor),
+                                            ChartData('title', 4, daXuLyColor)
                                           ],
                                           [
-                                            ChartData('title', 1, Colors.red),
+                                            ChartData('title', 3, choXuLyColor),
                                             ChartData(
-                                                'title', 11, Colors.black),
-                                            ChartData('title', 2, Colors.red)
-                                          ]
+                                                'title', 5, chuaThucHienColor),
+                                            ChartData(
+                                                'title', 6, dangThucHienColor),
+                                            ChartData('title', 4, daXuLyColor)
+                                          ],
+                                          [
+                                            ChartData('title', 1, choXuLyColor),
+                                            ChartData(
+                                                'title', 5, chuaThucHienColor),
+                                            ChartData(
+                                                'title', 7, dangThucHienColor),
+                                            ChartData('title', 8, daXuLyColor)
+                                          ],
+                                          [
+                                            ChartData('title', 3, choXuLyColor),
+                                            ChartData(
+                                                'title', 5, chuaThucHienColor),
+                                            ChartData(
+                                                'title', 2, dangThucHienColor),
+                                            ChartData('title', 10, daXuLyColor)
+                                          ],
+                                          [
+                                            ChartData('title', 7, choXuLyColor),
+                                            ChartData(
+                                                'title', 5, chuaThucHienColor),
+                                            ChartData(
+                                                'title', 4, dangThucHienColor),
+                                            ChartData('title', 3, daXuLyColor)
+                                          ],
+                                          [
+                                            ChartData(
+                                                'title', 12, choXuLyColor),
+                                            ChartData(
+                                                'title', 20, chuaThucHienColor),
+                                            ChartData(
+                                                'title', 21, dangThucHienColor),
+                                            ChartData('title', 2, daXuLyColor)
+                                          ],
+                                          [
+                                            ChartData('title', 3, choXuLyColor),
+                                            ChartData(
+                                                'title', 4, chuaThucHienColor),
+                                            ChartData(
+                                                'title', 10, dangThucHienColor),
+                                            ChartData('title', 14, daXuLyColor)
+                                          ],
+                                          [
+                                            ChartData(
+                                                'title', 14, choXuLyColor),
+                                            ChartData(
+                                                'title', 2, chuaThucHienColor),
+                                            ChartData(
+                                                'title', 5, dangThucHienColor),
+                                            ChartData('title', 0, daXuLyColor)
+                                          ],
+                                          [
+                                            ChartData(
+                                                'title', 11, choXuLyColor),
+                                            ChartData(
+                                                'title', 11, chuaThucHienColor),
+                                            ChartData(
+                                                'title', 20, dangThucHienColor),
+                                            ChartData('title', 10, daXuLyColor)
+                                          ],
+                                          [
+                                            ChartData('title', 1, choXuLyColor),
+                                            ChartData(
+                                                'title', 9, chuaThucHienColor),
+                                            ChartData(
+                                                'title', 22, dangThucHienColor),
+                                            ChartData('title', 10, daXuLyColor)
+                                          ],
+                                          [
+                                            ChartData('title', 3, choXuLyColor),
+                                            ChartData(
+                                                'title', 5, chuaThucHienColor),
+                                            ChartData(
+                                                'title', 2, dangThucHienColor),
+                                            ChartData('title', 1, daXuLyColor)
+                                          ],
+                                          [
+                                            ChartData(
+                                                'title', 15, choXuLyColor),
+                                            ChartData(
+                                                'title', 23, chuaThucHienColor),
+                                            ChartData(
+                                                'title', 1, dangThucHienColor),
+                                            ChartData('title', 0, daXuLyColor)
+                                          ],
                                         ],
                                         listStatusData: [
-                                          ChartData('title', 11, Colors.black),
-                                          ChartData('title', 111, Colors.red)
+                                          ChartData(
+                                            S.current.cho_phan_xu_ly,
+                                            0,
+                                            choXuLyColor,
+                                          ),
+                                          ChartData(
+                                            S.current.chua_thuc_hien,
+                                            0,
+                                            chuaThucHienColor,
+                                          ),
+                                          ChartData(
+                                            S.current.dang_thuc_hien,
+                                            0,
+                                            dangThucHienColor,
+                                          ),
+                                          ChartData(
+                                            S.current.da_thuc_hien,
+                                            0,
+                                            daXuLyColor,
+                                          ),
                                         ],
                                         listTitle: [
                                           //todo data
-                                          'fuck',
-                                          'fuck',
+                                          'Tháng 12',
+                                          'Tháng 11',
+                                          'Tháng 10',
+                                          'Tháng 9',
+                                          'Tháng 8',
+                                          'Tháng 7',
+                                          'Tháng 6',
+                                          'Tháng 5',
+                                          'Tháng 4',
+                                          'Tháng 3',
+                                          'Tháng 2',
+                                          'Tháng 1',
                                         ],
                                         cubit: cubit,
                                       )
@@ -280,7 +395,7 @@ class _BaoCaoThongKeNhiemVuMobileState
                             title: [
                               Expanded(
                                 child: Text(
-                                  S.current.nhiem_vu_don_vi_xu_ly,
+                                  S.current.bang_nhiem_vu_theo_don_vi_xu_ly,
                                   style: textNormalCustom(
                                     color: AppTheme.getInstance().titleColor(),
                                     fontSize: 16,

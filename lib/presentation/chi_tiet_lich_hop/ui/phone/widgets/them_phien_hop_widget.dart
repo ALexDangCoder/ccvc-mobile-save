06cb5c788dddeed5b531cc440d0b0de427443d2e
tungdang <dcutung01112001@gmail.com>
2022-07-27 15:49:38 +0700

    - Fix bug: https://jira.edsolabs.com/browse/A22DAMPIMOBI-1087. - Root case: Thêm phiên họp-Lỗi thông báo trống trường bắt buộc nhập. - Thông báo sai đúng format gồm Tên phiên họp: " Vui lòng nhập tên phiên họp" và Nội dung phiên họp: " Vui lòng nhập nội dung phiên họp".

--- lib/presentation/chi_tiet_lich_hop/ui/phone/widgets/them_phien_hop_widget.dart
+++ lib/presentation/chi_tiet_lich_hop/ui/phone/widgets/them_phien_hop_widget.dart
@@ -101,7 +101,9 @@ class _ThemPhienHopScreenState extends State<ThemPhienHopScreen> {
                       taoPhienHopRequest.tieuDe = value;
                     },
                     validator: (value) {
-                      return value?.checkNull();
+                      return value?.checkNull(
+                        showText: S.current.vui_long_nhap_ten_phien_hop,
+                      );
                     },
                   ),
                 ),
@@ -157,7 +159,9 @@ class _ThemPhienHopScreenState extends State<ThemPhienHopScreen> {
                       taoPhienHopRequest.noiDung = value;
                     },
                     validator: (value) {
-                      return value?.checkNull();
+                      return value?.checkNull(
+                        showText: S.current.vui_long_nhap_noi_dung_phien_hop,
+                      );
                     },
                   ),
                 ),

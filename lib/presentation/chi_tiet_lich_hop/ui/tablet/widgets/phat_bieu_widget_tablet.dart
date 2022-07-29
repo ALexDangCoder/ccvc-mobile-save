70ddedc3a7b23d35328de0f37394463d56e1b11c
hungtv <hungtv@edsolabs.com>
2022-07-29 15:22:59 +0700

    add size phat bieu lich hop (namlv)

--- lib/presentation/chi_tiet_lich_hop/ui/tablet/widgets/phat_bieu_widget_tablet.dart
+++ lib/presentation/chi_tiet_lich_hop/ui/tablet/widgets/phat_bieu_widget_tablet.dart
@@ -302,26 +302,15 @@ Widget buttonDuyet(
         ),
       );
     case StatePhatBieu.da_duyet:
-      return Padding(
-        padding: EdgeInsets.only(
-          right: isMobile() ? 250 : 350,
-          left: isMobile() ? 0 : 350,
-        ),
-        child: SizedBox(
-          child:
-              buttomHuyPb(context: context, cubit: cubit, isTablet: isTablet),
-        ),
+      return SizedBox(
+        width: 120,
+        child: buttomHuyPb(context: context, cubit: cubit, isTablet: isTablet),
       );
     case StatePhatBieu.huy_duyet:
-      return Padding(
-        padding: EdgeInsets.only(
-          right: isMobile() ? 250 : 350,
-          left: isMobile() ? 0 : 350,
-        ),
-        child: SizedBox(
-          child:
-              buttomDuyetPb(context: context, cubit: cubit, isTablet: isTablet),
-        ),
+      return SizedBox(
+        width: 120,
+        child:
+            buttomDuyetPb(context: context, cubit: cubit, isTablet: isTablet),
       );
   }
   return const SizedBox();

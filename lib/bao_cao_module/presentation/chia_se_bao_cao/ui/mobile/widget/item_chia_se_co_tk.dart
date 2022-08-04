6b3ea2d1fd9037c6e72786e9750eec8d43d83dc3
HoaVu <hoavd@edsolabs.com>
2022-08-04 09:33:40 +0700

    sua mau checkbox theo mau theme

--- lib/bao_cao_module/presentation/chia_se_bao_cao/ui/mobile/widget/item_chia_se_co_tk.dart
+++ lib/bao_cao_module/presentation/chia_se_bao_cao/ui/mobile/widget/item_chia_se_co_tk.dart
@@ -117,7 +117,7 @@ class _ItemChiaSeCoTkState extends State<ItemChiaSeCoTk> {
               height: 20,
               child: Checkbox(
                 checkColor: AppTheme.getInstance().backGroundColor(),
-                activeColor: textDefault,
+                activeColor: AppTheme.getInstance().colorField(),
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(3),
                 ),

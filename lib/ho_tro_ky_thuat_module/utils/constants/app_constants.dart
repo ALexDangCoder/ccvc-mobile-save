7542b49fb2f69695597237ce614e0c8e4be04c8f
hungnguyen <hungnguyen0904199x@gmail.com>
2022-08-17 09:44:27 +0700

    - fix bug 1501

--- lib/ho_tro_ky_thuat_module/utils/constants/app_constants.dart
+++ lib/ho_tro_ky_thuat_module/utils/constants/app_constants.dart
@@ -14,12 +14,15 @@ const _dtFormat9 = 'yyyy/MM//dd HH:mm:ss.ss';
 const _dtFormat10 = 'yyyy-MM-dd HH:mm:ss.ss';
 const _dtFormat8 = 'dd/MM/yyyy';
 const _dtFormatUpperCase = 'DD/MM/YYYY';
+const _dtLowCase = 'dd/mm/yyyy';
 
 class DateTimeFormat {
   static const DATE_BE_RESPONSE_FORMAT = _dtFormat7;
   static const DATE_FORMAT_TEXT_FIELD = _dtFormatUpperCase;
   static const DATE_SELECT_TEXT_FIELD = _dtFormat9;
   static const DATE_ISO_86 = _dtFormat10;
+  static const DATE_TIME_LOW_CASE = _dtLowCase;
+  static const DATE_TIME_FORMAT_8 = _dtFormat8;
 }
 
 const String successCode = '200';

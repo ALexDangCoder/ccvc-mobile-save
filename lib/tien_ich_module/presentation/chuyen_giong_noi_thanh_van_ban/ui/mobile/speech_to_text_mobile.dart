d887d4424a8086a0d9dff942906aa8a6c72cb412
NamLV <namlv@edsolabs.com>
2022-08-25 12:06:48 +0700

    [Lịch làm việc_Mobile/Ipad/Tablet] - Lịch Thu hồi vẫn hiển thị button more

--- lib/tien_ich_module/presentation/chuyen_giong_noi_thanh_van_ban/ui/mobile/speech_to_text_mobile.dart
+++ lib/tien_ich_module/presentation/chuyen_giong_noi_thanh_van_ban/ui/mobile/speech_to_text_mobile.dart
@@ -199,9 +199,6 @@ class _SpeechToTextMobileState extends State<SpeechToTextMobile> {
                   ),
                   GestureDetector(
                     onTap: () {
-                      if (!_hasSpeech) {
-                        return;
-                      }
                       !isListening ? startListening() : stopListening();
                       cubit.isVoiceSubject.sink.add(speech.isListening);
                     },
